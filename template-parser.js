a04ee415a654165b4c24d58f1f6303bfb4d9ba0f
eric <eric@gregarious.com.au>
2018-03-09 18:34:27 +1000

    force wss:// protocol if crypto cert is supplied on cmd line

--- template-parser.js
+++ template-parser.js
@@ -1,7 +1,7 @@
 const Mustache = require("mustache");
 const fs = require("fs");
 
-function api(root, svc) {
+function api(root, svc, wsproto) {
 	var output = "var " + svc + " = {}; \n" + svc + ".metadata = {};  \n";
 	const template = fs.readFileSync(__dirname + '/template.txt', 'utf8');
 	const svcdata = root.lookup(svc);
@@ -16,7 +16,8 @@ function api(root, svc) {
 
 		var view = {
 			method: name,
-			service: svc
+			service: svc,
+			ws: wsproto
 		};
 
 		output += Mustache.render(template, view);
